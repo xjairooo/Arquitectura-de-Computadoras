561fb5cff54b70eb999ded9105fcc98f26f271fc
Jairo Campero <camperojairo0@gmail.com>
2025-04-04 19:58:35 -0300

    fix: ej2 completado

--- 2 - Unidad/Practica/TP3/Asignacion de Registros.asm
+++ 2 - Unidad/Practica/TP3/Asignacion de Registros.asm
@@ -1,14 +1,18 @@
-#Asignación de registros: Escribir un programa que asigne los siguientes valores a
-#los registros indicados: x3=3, x4=4, x5=5, x6=6, x7=x7 y x8=8. Ejecutarlo paso a
-#paso para comprobar que funciona correctamente.
+# Asignación de registros: Escribir un programa que asigne los siguientes valores
+#a los registros indicados: x3=3, x4=4, x5=5, x6=6, x7=x7 y x8=8. Ejecutarlo paso
+#a paso para comprobar que funciona correctamente.
 
-
-	.data
-num1:	la x3, 3	
+	
 	.text
 main:
+	# li = load inmediate
+	li x3, 3  # x3 = 3 (pseudoinstruccion que se expande a addi x3, x0, 3)
+	li x4, 4  # x4 = 4  
+	li x5, 5  # x5 = 5
+	li x6, 6  # x6 = 6
+	li x7, 7  # x7 = 7
+	li x8, 8  # x8 = 8
 	
-	li t1, num1
-	
-	li a7, 10
+	# Terminacion del programa
+	li a7, 10  # Codigo syscall exit(10) 
 	ecall
