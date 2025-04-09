cd96f8104510829489bdb49272263d60231c4115
Jairo Campero <camperojairo0@gmail.com>
2025-04-08 23:45:08 -0300

    tp4 Add MUL con subrutina y pasaje de valor

--- 2 - Unidad/Practica/TP4/MUL c-sub y pasaje de valor.asm
+++ 2 - Unidad/Practica/TP4/MUL c-sub y pasaje de valor.asm
@@ -1,3 +1,6 @@
+# Por Valor = pasa el contenido del dato y NO se puede modificar
+# NUM1 : 6 y NUM2: 7, se pasan esos datos
+# Por Referencia: pasa la direccion de esos datos y SI se puede modificar
 	.data
 NUM1: .word 6
 NUM2: .word 7
@@ -10,15 +13,15 @@ msg3: .string "\nResultado = "
 
 main:
     la t0, NUM1
-    lw t1, 0(t0)
+    lw a0, 0(t0)
 
     la t0, NUM2
-    lw t2, 0(t0)
+    lw a1, 0(t0)
 
-    mul t3, t1, t2
+    call MUL
 
     la t0, RES
-    sw t3, 0(t0)
+    sw a2, 0(t0)
 
     # Mostrar NUM1
     li a7, 4
@@ -26,7 +29,8 @@ main:
     ecall
 
     li a7, 1
-    mv a0, t1
+    la t0, NUM1
+    lw a0, 0(t0)
     ecall
 
     # Mostrar NUM2
@@ -35,7 +39,8 @@ main:
     ecall
 
     li a7, 1
-    mv a0, t2
+    la t0, NUM2
+    lw a0, 0(t0)
     ecall
 
     # Mostrar resultado
@@ -44,8 +49,13 @@ main:
     ecall
 
     li a7, 1
-    mv a0, t3
+    mv a0, a2
     ecall
 
     li a7, 10
     ecall
+
+# SUBRUTINA MUL (por valor)
+MUL:
+    mul a2, a0, a1
+    ret
